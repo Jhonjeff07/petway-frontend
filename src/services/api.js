d49adcc5d3855c9f41ceb9a98b96128817443e9c
Jhonjeff07 <jhonatan.castrod@cun.edu.co>
2025-09-06 15:53:58 -0500

    Usar variable de entorno para la URL de la API

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,11 @@
 import axios from "axios";
 
 // 🔹 Configuración base de Axios
+import axios from "axios";
+
+// 🔹 Configuración base de Axios
 const api = axios.create({
-  baseURL: "http://localhost:4000/api",
+  baseURL: import.meta.env.VITE_API_URL + "/api", // Usar la variable de entorno
   withCredentials: true,
 });
 
